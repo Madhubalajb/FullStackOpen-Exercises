dd7bbfab0266fca97a9e991e010054da19dec3de
Madhubalajb <jbmadhumng@gmail.com>
2019-06-11 15:16:08 +0530

    Add Course contents step9

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './Header';
 import Content from './Content';
 
+const coursecontent = (props) => props.course.map (course =>
+    <div>
+        <Header name={course.name} />
+        <Content parts={course.parts} />   
+    </div>  
+)
+
 const Course = (props) => {
     return (
-        <div>
-            <Header name={props.course.name} />
-            <Content parts={props.course.parts} />
-        </div>   
+        coursecontent(props)
     )
 }
 
