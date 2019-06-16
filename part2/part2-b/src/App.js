fe78ed362f00009ba75bfe64d067921747f7e7e6
Madhubalajb <jbmadhumng@gmail.com>
2019-06-16 17:29:57 +0530

    Add the phonebook step2

--- part2/part2-b/src/App.js
+++ part2/part2-b/src/App.js
@@ -2,26 +2,31 @@ import React, {useState} from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', id: 1}
   ]);
   const [newPerson, setNewPerson] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
-    const person = {
-      name: newPerson,
-      id: persons.length + 1
+    const check = persons.some(per => per.name === newPerson);
+    if (!check) {
+      const person = {
+        name: newPerson,
+        id: persons.length + 1
+      }
+      setPersons(persons.concat(person));
+      setNewPerson(''); 
     }
-    setPersons(persons.concat(person));
-    setNewPerson('');
+    else {
+      alert(`${newPerson} is already added to phonebook`)
+    }
   }
 
   const handleNewPerson = (event) => {
-    console.log(event.target.value)
     setNewPerson(event.target.value)
   }
 
-  const phoneBook = () => persons.map(per => <p>{ per.name }</p>)
+  const phoneBook = () => persons.map(per => <p key={per.id}>{per.name}</p>)
 
   return (
     <div>
