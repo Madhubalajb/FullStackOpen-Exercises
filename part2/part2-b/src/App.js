1cce6777cd994ec5bb9d1eb20f03ae158221ddf2
Madhubalajb <jbmadhumng@gmail.com>
2019-06-17 06:54:03 +0530

    Add The Phonebook step4

--- part2/part2-b/src/App.js
+++ part2/part2-b/src/App.js
@@ -8,10 +8,12 @@ const App = () => {
     { name: 'Arto Hellas', number: '0987654', id: 1},
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+    { name: 'Madhu', number: '67890456', id: 5}
   ]);
   const [newPerson, setNewPerson] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState(''); 
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -27,20 +29,23 @@ const App = () => {
       setNewNumber('');
     }
     else {
-      alert(`${newPerson} is already added to phonebook`)
+      alert(`${newPerson} is already added to phonebook`);
     }
   }
-  const handleNewPerson = (event) => setNewPerson(event.target.value)
+  const handleNewPerson = (event) => setNewPerson(event.target.value);
   const handleNewNumber = (event) => setNewNumber(event.target.value);
-  
-  const phoneBook = () => persons.map(per => <p key={per.id}>{per.name} {per.number} </p>)
+  const handleFilter = (event) => setFilter(event.target.value);
+  let filteredItems = persons.filter(
+    (contact) => contact.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1
+  );
+  const phoneBook = () => filteredItems.map(per => <p key={per.id}>{per.name} {per.number} </p>);
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter add={addPerson} person={handleNewPerson} number={handleNewNumber} />
+      <Filter filters={handleFilter} />
       <h2>add a new</h2>
-      <PersonForm />
+      <PersonForm add={addPerson} person={handleNewPerson} number={handleNewNumber} />
       <h2>Numbers</h2>
       <Persons func={phoneBook()} />
     </div>
--- part2/part2-b/src/components/Filter.js
+++ part2/part2-b/src/components/Filter.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Filter = (props) => {
     return (
-        <div>filter shown with <input /></div>
+        <div>filter shown with <input onChange={props.filters} /></div>
     )
 }
 
