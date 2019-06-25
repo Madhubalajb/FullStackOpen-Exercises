c7fff20cfd534452267eb56301ffa4f177d9393f
Madhubalajb <jbmadhumng@gmail.com>
2019-06-25 10:13:47 +0530

    step10 2.18

--- part2/part2-b(phone_book)/src/App.js
+++ part2/part2-b(phone_book)/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import personServices from './services/phonebook';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
@@ -11,33 +11,51 @@ const App = () => {
   const [filter, setFilter] = useState(''); 
 
   useEffect(() => {
-    axios
-      .get(' http://localhost:3001/persons')
-      .then(response => setPersons(response.data))
+     personServices.getData().then(persons => {setPersons(persons)})
   }, [])
 
   const addPerson = (event) => {
     event.preventDefault();
-    const check = persons.some(per => per.name === newPerson);
+    const check = persons.some(per => per.name === newPerson)
     if (!check) {
       const person = {
         name: newPerson,
         number: newNumber,
         id: persons.length + 1
       }
-      setPersons(persons.concat(person));
-      setNewPerson(''); 
-      setNewNumber('');
+      personServices.addData(person).then(returnedPersons => {
+        setPersons(persons.concat(returnedPersons))
+        setNewPerson('')
+        setNewNumber('')
+      })
     }
     else {
-      alert(`${newPerson} is already added to phonebook`);
+      window.confirm(`${newPerson} is already added to phonebook, replace the old number with the new one?`)
+      const update = persons.find(p => p.name === newPerson)
+      const updateObject = { ...update, number: newNumber}
+      personServices.updateData(updateObject.id, updateObject).then(updated => {
+        setPersons(persons.map(pp => pp.id !== updateObject.id ? pp : updated))
+      })
     }
   }
-  const handleNewPerson = (event) => setNewPerson(event.target.value);
-  const handleNewNumber = (event) => setNewNumber(event.target.value);
-  const handleFilter = (event) => setFilter(event.target.value);
+  const removePerson = (person) => {
+    window.confirm(`Delete ${person.name} ?`)
+    personServices.deleteData(person.id)
+    .then(deleted => {
+      setPersons(persons.filter(per => per.id !== person.id))
+    })
+    .catch(error => {
+      console.log("fail")
+    })
+  }
+  const handleNewPerson = (event) => setNewPerson(event.target.value)
+  const handleNewNumber = (event) => setNewNumber(event.target.value)
+  const handleFilter = (event) => setFilter(event.target.value)
   let filteredItems = persons.filter(contact => contact.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1)
-  const phoneBook = () => filteredItems.map(per => <p key={per.id}>{per.name} {per.number} </p>);
+  const phoneBook = () => filteredItems.map(per => 
+      <p key={per.id}>
+        {per.name}: {per.number} <button onClick={() => {removePerson(per)}}>Delete</button>
+      </p>);
 
   return (
     <div>
