147c8051f8810681e809558a1ac85c2485dc9311
Madhubalajb <jbmadhumng@gmail.com>
2020-03-27 23:23:12 +0530

    Add Icons

--- part2/part2-b(phone_book)/src/components/Persons.js
+++ part2/part2-b(phone_book)/src/components/Persons.js
@@ -3,13 +3,14 @@ import React from 'react'
 const Persons = (props) => {
     const filteredItems = props.filteredItems
 
-    const phoneBook  = () => filteredItems.map(per => 
+    const phoneBook  = () => filteredItems.map(per => {
         <div key={per.id}> 
             <i className="material-icons">assignment_ind</i>
                 {per.name}
                 {per.number} 
             <i class="material-icons" onClick={() => props.removePerson(per)}>delete</i>
-        </div>)
+        </div>
+    })
   
     return (
         <div>{phoneBook()}</div>
