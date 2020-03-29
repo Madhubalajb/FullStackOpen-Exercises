481fcd89074ded99322097c4894295e78d0918e6
Madhubalajb <jbmadhumng@gmail.com>
2020-03-29 17:51:11 +0530

    style empty contacts

--- part2/part2-b(phone_book)/src/components/Persons.js
+++ part2/part2-b(phone_book)/src/components/Persons.js
@@ -4,27 +4,28 @@ const Persons = (props) => {
     const filteredItems = props.filteredItems
 
     const phoneBook  = () => filteredItems.map(per => { 
-        if (filteredItems === []) {
-            return (
-                <p>
-                    None, Try adding some :)
-                </p>
-            )
-        }
-        else {
-            return (
-                <div key={per.id} className="contact">
-                    <i className="material-icons person">assignment_ind</i> 
-                    <span className="name">{per.name}</span> - <span className="number">{per.number}</span>
-                    <i className="material-icons delete" title="Delete Contact" onClick={() => props.remove(per)}>delete</i>
-                </div>
-            )
-        }
+        return (
+            <div key={per.id} className="contact">
+                <i className="material-icons person">assignment_ind</i>
+                <span className="name">{per.name}</span> - <span className="number">{per.number}</span>
+                <i className="material-icons delete" title="Delete Contact" onClick={() => props.remove(per)}>delete</i>
+            </div>
+        )
     })
-  
-    return (
+    
+    if (filteredItems.length === 0) {
+        return (
+            <div>
+                <i className="material-icons">call_end</i>
+                <p className="none">None, create some by logging In / Signing Up...</p>
+            </div>
+        )
+    }
+    else {
+        return (
         <div>{phoneBook()}</div>
-    )
+        )
+    }
 }
 
-export default Persons;
+export default Persons
