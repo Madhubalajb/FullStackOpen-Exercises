8e6b4a035828a02c6513a050b9ad2ddf4a39461c
Madhubalajb <jbmadhumng@gmail.com>
2020-03-29 07:46:15 +0530

    Debug

--- part2/part2-b(phone_book)/src/services/phoneService.js
+++ part2/part2-b(phone_book)/src/services/phoneService.js
@@ -21,12 +21,18 @@ const addData = async (person) => {
 }
 
 const deleteData = async (person_id) => {
-    const response = await axios.delete(`${url}/${person_id}`)
+    const config = {
+        headers: { Authorization: token }
+    }
+    const response = await axios.delete(`${url}/${person_id}`, config)
     return response.data
 }
 
 const updateData = async (person_id, person) => {
-    const response = await axios.put(`${url}/${person_id}`, person);
+    const config = {
+        headers: { Authorization: token }
+    }
+    const response = await axios.put(`${url}/${person_id}`, person, config);
     return response.data
 }
 
